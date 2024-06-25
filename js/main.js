703dd274cf6ea96467732d7359c278647381c017
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-25 23:38:05 +0530

    script

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// functionality
+
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+//let list = document.querySelectorAll(".list");
+let list = document.getElementsByClassName("list");
+
+
+
