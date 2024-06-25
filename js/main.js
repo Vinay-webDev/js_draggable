2fa29e5fec083e9ceab5517d42c8cf90c89a60ae
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-25 23:49:00 +0530

    not working:((((

--- js/main.js
+++ js/main.js
@@ -7,5 +7,16 @@ let lists = document.getElementsByClassName("list");
 //  make it lists ***
 // for loop 
 
-
+for (list of lists) {
+    list.addEventListener("dragstart", (event) => {
+        let selected = event.target;
+        right.addEventListener("dragover", (event) => {
+            event.preventDefault();
+        })
+        right.addEventListener("drop", (event) => {
+            right.appendChild(selected);
+            selected = null;
+        })
+    })
+}
 
