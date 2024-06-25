7e434c2ca943da62a237f2a1947d7243ea888f08
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-25 23:55:52 +0530

    fxxk it's not working maybe I'll try later😤😫😫😩😩

--- js/main.js
+++ js/main.js
@@ -6,17 +6,42 @@ let right = document.getElementById("right");
 let lists = document.getElementsByClassName("list");
 //  make it lists ***
 // for loop 
-
+/*
 for (list of lists) {
-    list.addEventListener("dragstart", (event) => {
+    list.addEventListener("dragstart", function(event) {
         let selected = event.target;
-        right.addEventListener("dragover", (event) => {
+        right.addEventListener("dragover", function(event) {
             event.preventDefault();
         })
-        right.addEventListener("drop", (event) => {
+        right.addEventListener("drop", function(event) {
             right.appendChild(selected);
             selected = null;
         })
     })
 }
+*/
+for (lists of lists) {
+    lists.addEventListener("dragstart", function(event) {
+        let selected = event.target;
+            right.addEventListener("dragover", function(event) {
+                event.preventDefault();
+            })
+            right.addEventListener("drop", function(event) {
+                right.appendChild(selected);
+                selected = null;
+            })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
